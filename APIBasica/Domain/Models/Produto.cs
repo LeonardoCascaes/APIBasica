484ff22fd1ec27556847457ae383ca9ebd74b81d
leonardo <leonardo@zuplae.com>
2021-12-11 20:01:47 -0300

    Criado contexto para conexão e configurations para mapeamento dos objetos conforme entidades no database

--- APIBasica/Domain/Models/Produto.cs
+++ APIBasica/Domain/Models/Produto.cs
@@ -5,13 +5,22 @@
         public string Nome { get; set; }
         public float Peso { get; set; }
         public int CategoriaId { get; set; }
-        public Categoria Categoria { get; set; }
+        public Categoria? Categoria { get; set; }
+        public DateTime DataCadastro { get; set; }
 
         public Produto(string nome, float peso, Categoria categoria)
         {
             Nome = nome;
             Peso = peso;
             Categoria = categoria;
+            DataCadastro = DateTime.Now;
+        }
+
+        public Produto(string nome, float peso)
+        {
+            Nome = nome;
+            Peso = peso;
+            DataCadastro = DateTime.Now;
         }
     }
 }
